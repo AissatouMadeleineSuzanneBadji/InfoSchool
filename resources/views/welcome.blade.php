3b734a25948d9f0932df05edf4c560a4b864b0df
Badji <suzannebadji1@gmail.com>
2019-11-28 15:13:00 +0000

    mom commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>InfoSchool</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    InfoSchool
                 </div>
 
                 <div class="links">
